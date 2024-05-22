231dbdf948bf1a6f6b4dd761741f997a3530dbc9
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-22 23:19:32 +0300

    folders structure fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Charge from "@/components/Charge";
-import Counters from "@/components/Counters";
-import Customers from "@/components/Customers";
-import Doyoulikefaces from "@/components/Doyoulikefaces";
-import Mission from "@/components/Mission";
-import TripleImages from "@/components/TripleImages";
-import Whatwedo from "@/components/Whatwedo";
-import Whatwedontknow from "@/components/Whatwedontknow";
+import Charge from "@/components/Maincomps/Charge";
+import Counters from "@/components/Maincomps/Counters";
+import Customers from "@/components/Maincomps/Customers";
+import Doyoulikefaces from "@/components/Maincomps/Doyoulikefaces";
+import Mission from "@/components/Maincomps/Mission";
+import TripleImages from "@/components/Maincomps/TripleImages";
+import Whatwedo from "@/components/Maincomps/Whatwedo";
+import Whatwedontknow from "@/components/Maincomps/Whatwedontknow";
 
 export default function Home() {
   return (
