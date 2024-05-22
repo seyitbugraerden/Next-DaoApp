d0f25c0f4b72c69ca49e55c41e4e7a11addb384c
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-22 23:12:52 +0300

    Tailwind implement & header fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,11 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <nav
-        id="navScroll"
-        className="navbar navbar-dark bg-black fixed-top px-vw-5"
-      >
-        <div className="container">
+      <nav id="navScroll" className=" bg-dark text-white py-2">
+        <div className="container flex flex-row justify-between items-center">
           <Link
-            className="navbar-brand pe-md-4 fs-4 col-12 col-md-auto text-center"
-            href="index.html"
+            className="text-white flex flex-row items-center gap-2 no-underline"
+            href="/"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -24,32 +21,28 @@ function Header() {
               <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.598.598 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.598.598 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.598.598 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535L7.733.063z" />
               <path d="m14.12 6.576 1.715.858c.22.11.22.424 0 .534l-7.568 3.784a.598.598 0 0 1-.534 0L.165 7.968a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.659z" />
             </svg>
-            <span className="ms-md-1 mt-1 fw-bolder me-md-5">Klar</span>
+            <span className="text-bold text-3xl !no-underline">Klar</span>
           </Link>
 
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 list-group list-group-horizontal">
-            <li className="nav-item">
-              <Link
-                className="nav-link fs-5"
-                href="index.html"
-                aria-label="Homepage"
-              >
+          <ul className="flex flex:md-row  items-center justify-center mb-0 gap-3">
+            <li className="">
+              <Link className="no-underline text-white hover:opacity-80" href="/" aria-label="Homepage">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="">
               <Link
-                className="nav-link fs-5"
-                href="content.html"
+                className="no-underline text-white hover:opacity-80"
+                href="/content"
                 aria-label="A sample content page"
               >
                 Content
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="">
               <Link
-                className="nav-link fs-5"
-                href="system.html"
+                className="no-underline text-white hover:opacity-80"
+                href="/system"
                 aria-label="A system message page"
               >
                 System
@@ -65,7 +58,7 @@ function Header() {
           </Link>
         </div>
       </nav>
-      ;
+      
     </header>
   );
 }
