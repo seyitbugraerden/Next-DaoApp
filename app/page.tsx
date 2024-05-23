343abafa91c99636982b69a7f3660f86c374bc7f
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-24 00:06:52 +0300

    fatal function cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,8 @@ import Mission from "@/components/Maincomps/Mission";
 import TripleImages from "@/components/Maincomps/TripleImages";
 import Whatwedo from "@/components/Maincomps/Whatwedo";
 import Whatwedontknow from "@/components/Maincomps/Whatwedontknow";
-import { pb } from "@/lib/pb";
 
 export default async function Home() {
-  const records = await pb.collection('users').getFullList()
-  console.log(records);
   return (
     <>
       <Mission />
