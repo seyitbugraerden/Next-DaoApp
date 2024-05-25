b948cecb700926b5d9c8be6d1fdbf25e94e361b9
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-25 23:32:35 +0300

    fetch clears

--- components/fetch/fetch.ts
+++ components/fetch/fetch.ts
@@ -1,19 +1,5 @@
 import { pb } from "@/lib/pb";
 
-export const fetchBlogData = async () => {
-  try {
-    const response = await fetch(
-      "https://json-placeholder.mock.beeceptor.com/posts"
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-};
 
 export const fetchBlog = async () => {
   return await pb.collection("Blogs").getFullList();
