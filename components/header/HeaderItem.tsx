36f1107a318185bfe4614321e13309ac44b8999d
seyitbugraerden <seyitbugraerden@gmail.com>
2024-06-16 02:13:33 +0300

    header icon added

--- components/header/HeaderItem.tsx
+++ components/header/HeaderItem.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
 import React from "react";
+import logout from "@/public/logout.svg";
+import Image from "next/image";
 import { header } from "../../types/header";
 
 const HeaderItem: React.FC<header> = ({ item }) => {
   return (
     <li>
-      <Link
-        href={`${item.link}`}
-        className="no-underline text-white hover:opacity-70 duration-500"
-        aria-label="Homepage"
-      >
-        {item.title}
-      </Link>
+      {item.title === "User" ? (
+        <Image src={logout} alt="Log Out" height={30} className="hover:opacity-50 transiiton duration-300 cursor-pointer"/>
+      ) : (
+        <Link
+          href={`${item.link}`}
+          className="no-underline text-white hover:opacity-70 duration-500"
+          aria-label="Homepage"
+        >
+          {item.title}
+        </Link>
+      )}
     </li>
   );
 };
