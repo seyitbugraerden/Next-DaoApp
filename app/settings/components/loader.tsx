c394689618bd407941a266801306187c1a391cd2
seyitbugraerden <seyitbugraerden@gmail.com>
2024-06-18 03:01:14 +0300

    fix of loader

--- app/settings/components/loader.tsx
+++ app/settings/components/loader.tsx
@@ -1,27 +1,39 @@
-"use client";
-import React, { useState } from "react";
+'use client'
+import React, { useState, useEffect } from "react";
 import "ldrs/ring";
 import { reuleaux } from "ldrs";
+
 function Loader() {
-  reuleaux.register();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      reuleaux.register();
+    }
+  }, []);
+
   const [delay, setDelay] = useState(true);
-  setTimeout(() => {
-    setDelay(false);
-  }, 1000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDelay(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div
       className={`loader-section flex justify-center items-center w-screen h-screen top-0 left-0 fixed bg-black z-[9999] ${
         !delay && "hidden"
       }`}
     >
-      <l-reuleaux
-        size="37"
-        stroke="5"
-        stroke-length="0.15"
-        bg-opacity="0.1"
-        speed="1.2"
-        color="white"
-      ></l-reuleaux>
+      {typeof window !== "undefined" && (
+        <l-reuleaux
+          size="37"
+          stroke="5"
+          stroke-length="0.15"
+          bg-opacity="0.1"
+          speed="1.2"
+          color="white"
+        ></l-reuleaux>
+      )}
     </div>
   );
 }
